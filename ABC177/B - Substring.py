c719bb0a64615950cdb83cc7bdbdbe2ddcbcedcc
ren <ren.ren.1030@icloud.com>
2021-02-09 23:12:55 +0900

    177-AB

--- ABC177/B - Substring.py
+++ ABC177/B - Substring.py
@@ -1,5 +1,14 @@
-d, t, s = map(int, input().split())
-if d/s <= t:
-    print("Yes")
-else:
-    print("No")
+s = input()
+t = input()
+ls = len(s)
+lt = len(t)
+ans = lt
+
+for i in range(ls-lt+1):
+    num = 0
+    for j in range(lt):
+        if s[i+j] != t[j]:
+            num += 1
+    ans = min(ans, num)
+
+print(ans)
