6a1ea39e87dcd878ce6984df8af826d8dfadfd8d
ren <ren.ren.1030@icloud.com>
2021-03-05 22:45:39 +0900

    150-C

---
+++ ABC150/C - Count Order.py
@@ -0,0 +1,9 @@
+import itertools
+
+n = int(input())
+p = tuple(map(int, input().split()))
+q = tuple(map(int, input().split()))
+
+num_list = list(itertools.permutations(range(1,n+1)))
+
+print(abs(num_list.index(p)-num_list.index(q)))
